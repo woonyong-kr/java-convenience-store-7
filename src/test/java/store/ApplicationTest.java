5ef8b9492cdc8138fb8fcb517328876cc97ddf15
choi woo-nyong <woonyong.kr@gmail.com>
2025-12-01 15:17:00 +0900

    feat: 우아한테크코스 7기 편의점 미션 환경 구성
    - 우아한테크코스 7기 편의점 미션과 동일한 환경 구성

---
+++ src/test/java/store/ApplicationTest.java
@@ -0,0 +1,54 @@
+package store;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertNowTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ApplicationTest extends NsTest {
+    @Test
+    void 파일에_있는_상품_목록_출력() {
+        assertSimpleTest(() -> {
+            run("[물-1]", "N", "N");
+            assertThat(output()).contains(
+                "- 콜라 1,000원 10개 탄산2+1",
+                "- 콜라 1,000원 10개",
+                "- 오렌지주스 1,800원 9개 MD추천상품",
+                "- 탄산수 1,200원 5개 탄산2+1"
+            );
+        });
+    }
+
+    @Test
+    void 여러_개의_일반_상품_구매() {
+        assertSimpleTest(() -> {
+            run("[비타민워터-3],[물-2],[정식도시락-2]", "N", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈18,300");
+        });
+    }
+
+    @Test
+    void 기간에_해당하지_않는_프로모션_적용() {
+        assertNowTest(() -> {
+            run("[감자칩-2]", "N", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈3,000");
+        }, LocalDate.of(2024, 2, 1).atStartOfDay());
+    }
+
+    @Test
+    void 예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("[컵라면-12]");
+            assertThat(output()).contains("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
